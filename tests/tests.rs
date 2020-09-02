632eb67b5503a3bb378120bb1cd20f2cbf76b549
Luke Pearson <luke@dusk.network>
2020-09-02 16:41:44 +0200

    Move tests
    The tests have been moved to
    their own file so the API looks
    cleaner for this repo.

---
+++ tests/tests.rs
@@ -0,0 +1,40 @@
+// Copyright (c) DUSK NETWORK. All rights reserved.
+// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.”
+
+use eddsa::{Message, KeyPair, PublicKey};
+use dusk_bls12_381::Scalar;
+
+#[cfg(test)]
+mod integrations {
+    use super::*;
+    
+    #[test]
+    // TestSignVerify
+    fn sign_verify() {  
+        let keypair = KeyPair::new().unwrap();
+        let mut rng = rand::thread_rng();
+
+        let message = Message(Scalar::random(&mut rng));
+    
+        let a = keypair.sign(&message);
+        let b = a.verify(&message, &keypair.public_key);
+
+        assert!(b);
+    }
+
+    #[test]
+    // Test to see failure with wrong Public Key
+    fn test_wrong_keys() {
+        let keypair = KeyPair::new().unwrap();
+        let mut rng = rand::thread_rng();
+
+        let message = Message(Scalar::random(&mut rng));
+    
+        let a = keypair.sign(&message);
+        let b = a.verify(&message, &PublicKey::new().unwrap());
+
+        assert!(!b);
+    }
+
+
+}
