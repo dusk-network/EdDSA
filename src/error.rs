2924528587b60b0f6ff3bb74cc0b74bf10852240
Luke Pearson <luke@dusk.network>
2020-09-03 12:01:45 +0200

    Add error handling
    The error handling for
    `thiserror` is added in
    this commit.

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 // Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.”
 
-use std::io;
+
 use thiserror::Error;
 
 #[derive(Debug)]
